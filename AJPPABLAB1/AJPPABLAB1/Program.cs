8c721c9bf23b1375ac028d2f5938c054881c6f9c
Elladrian <oleksii.hudzishevskyi@gmail.com>
2023-03-04 23:07:11 +0100

    teoretical part in report

--- AJPPABLAB1/AJPPABLAB1/Program.cs
+++ AJPPABLAB1/AJPPABLAB1/Program.cs
@@ -1,20 +1,39 @@
-﻿namespace AJPPABLAB1
+﻿using System.Data.SqlClient;
+
+namespace AJPPABLAB1
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            try{
-                // Kod programu
-            }
-            catch(Exception ex){
-                // Kod programu wykonywany jeśli w bloku wyżej zostanie wychwycony Exception.
-                Console.WriteLine(ex.Message);
-            }
-            finally {
-                // Blok kodu wykonywany zawsze, nawet jeśli zostanie wychwycony Exception albo nie.
-            }
+            string connectionString = @"Server=LOCALHOST\LOCALDATABASE;Database=TestDatabase;Trusted_Connection=True";
+            string sqlExpression = "SELECT * FROM Users";
 
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                await connection.OpenAsync();
+
+                SqlCommand command = new SqlCommand(sqlExpression, connection);
+                SqlDataReader reader = await command.ExecuteReaderAsync();
+
+                if(reader.HasRows)
+                {
+                    string columnName1 = reader.GetName(0);
+                    string columnName2 = reader.GetName(1);
+                    string columnName3 = reader.GetName(2);
+
+                    Console.WriteLine($"{columnName1}\t{columnName2}\t{columnName3}");
+
+                    while(await reader.ReadAsync())
+                    {
+                        var id = reader.GetValue(0);
+                        var name = reader.GetValue(1);
+                        var age = reader.GetValue(2);
+
+                        Console.WriteLine($"{id}\t{name}\t{age}");
+                    }
+                }
+            }
         }
     }
 }
