bd305448c787239c9b0c290070e64f986346677e
Elladrian <oleksii.hudzishevskyi@gmail.com>
2023-03-04 20:39:33 +0100

    solution created, report modyfications

---
+++ AJPPABLAB1/AJPPABLAB1/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace AJPPABLAB1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try{
+                // Kod programu
+            }
+            catch(Exception ex){
+                // Kod programu wykonywany jeśli w bloku wyżej zostanie wychwycony Exception.
+                Console.WriteLine(ex.Message);
+            }
+            finally {
+                // Blok kodu wykonywany zawsze, nawet jeśli zostanie wychwycony Exception albo nie.
+            }
+
+        }
+    }
+}
